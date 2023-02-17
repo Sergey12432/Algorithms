66c38102ac53942aac30334dd74669959fc2bed8
Alex FFFF <admin@admin.ru>
2023-02-17 10:05:17 +0300

    add factorual

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Factorial {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= n ; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+    }
 }
